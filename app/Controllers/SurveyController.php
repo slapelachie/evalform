e4775cc1bc2addba49e215a9cfc54a9b4044360d
Lachlan Slape <s4638520@student.uq.edu.au>
2024-04-13 23:14:33 +0000

    add survey view

--- app/Controllers/SurveyController.php
+++ app/Controllers/SurveyController.php
@@ -7,9 +7,22 @@ use CodeIgniter\HTTP\ResponseInterface;
 
 class SurveyController extends BaseController
 {
-    public function index()
+    public function index($survey_id)
     {
-        //
+        $surveysModel = new \App\Models\SurveysModel();
+        $questionsModel = new \App\Models\QuestionsModel();
+        $questionAnswerChoicesModel = new \App\Models\QuestionAnswerChoicesModel();
+
+        $data['survey'] = $surveysModel->where('id', $survey_id)->first();
+        $data['questions'] = $questionsModel->where('survey_id', $survey_id)->orderBy('question_number', 'ASC')->findAll();
+
+        foreach($data['questions'] as &$question)
+        {
+            $question['choices'] = $questionAnswerChoicesModel->where('question_id', $question['id'])->orderBy('position', 'ASC')->findAll();
+        }
+        unset($question);
+
+        return view('survey', $data);
     }
 
     public function manage($survey_id)
@@ -31,5 +44,10 @@ class SurveyController extends BaseController
         // TODO: Need to get survey response stuff for stats
 
         return view('survey_manage', $data);
+    }
+
+    public function edit($survey_id)
+    {
+        //
     }
 }
---
+++ app/Views/survey.php
@@ -0,0 +1,33 @@
+<?= $this->extend('base_template') ?>
+<?= $this->section('content') ?>
+
+<section class="py-3">
+    <div class="container">
+        <h1 class="display-5 mb-3"><?= $survey['name'] ?></h1>
+        <p><?= $survey['description'] ?></p>
+
+        <form class="bg-light rounded p-3">
+            <?php foreach ($questions as $question) : ?>
+                <div class="mb-3">
+                    <?php if ($question['type'] == 'multiple_choice') { ?>
+                        <p class="lead mb-2"><?= $question['question_number'] . '. ' . $question['question'] ?></p>
+                        <?php foreach ($question['choices'] as $choice) : ?>
+                            <div class="form-check mb-3">
+                                <label for="question-<?= $question['question_number'] . '-' . $choice['position'] ?>" class="form-check-label">
+                                    <?= $choice['answer'] ?>
+                                </label>
+                                <input type="radio" name="question-<?= $question['question_number'] ?>" class="form-check-input" id="question-<?= $question['question_number'] . '-' . $choice['position'] ?>">
+                            </div>
+                        <?php endforeach; ?>
+                    <?php } elseif ($question['type'] == 'free_text') { ?>
+                        <label for="question-<?= $question['question_number'] ?>" class="lead form-label"><?= $question['question_number'] . '. ' . $question['question'] ?></label>
+                        <textarea name="question-<?= $question['question_number'] ?>" id="question-<?= $question['question_number'] ?>" rows="3" class="form-control"></textarea>
+                    <?php } ?>
+                </div>
+            <?php endforeach; ?>
+            <button type="button" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</section>
+
+<?= $this->endSection() ?>
