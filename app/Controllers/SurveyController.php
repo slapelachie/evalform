ea64cf90c548d3ceb100748bf8702395b0725e1e
Lachlan Slape <slapelachie@gmail.com>
2024-04-30 10:00:44 +1000

    remove redundant code from manage controller

--- app/Controllers/SurveyController.php
+++ app/Controllers/SurveyController.php
@@ -44,35 +44,11 @@ class SurveyController extends BaseController
     {
         // TODO: Need to check permissions
         $surveysModel = new \App\Models\SurveysModel();
-        $questionsModel = new \App\Models\QuestionsModel();
-        $answersModel = new \App\Models\AnswersModel();
-
-        $questionResponsesModel = new \App\Models\QuestionResponsesModel();
 
         $data['survey'] = $surveysModel->find($surveyId);
         if ($data['survey'] == null) {
             throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound("This survey could not be found!");
         }
-
-        $data['questions'] = $questionsModel->where('survey_id', $surveyId)->orderBy('question_number', 'ASC')->findAll();
-
-        foreach ($data['questions'] as &$question) {
-            $questionId = $question['id'];
-            $question['choices'] = $answersModel->where('question_id', $questionId)->orderBy('position', 'ASC')->findAll();
-
-            $totalResponseCount = count($questionResponsesModel->where('question_id', $questionId)->findAll());
-
-            foreach ($question['choices'] as &$answer) {
-                $answerId = $answer['id'];
-                $answerResponseCount = count($questionResponsesModel->where('answer_id', $answerId)->findAll());
-                $data['responses'][$answerId]['count'] = $answerResponseCount;
-                $data['responses'][$answerId]['percent'] = $totalResponseCount != 0 ? round($answerResponseCount / $totalResponseCount * 100) : 'N/A';
-            }
-        }
-
-        unset($question);
-
-        // TODO: Need to get survey response stuff for stats
 
         return view('survey_manage', $data);
     }
